3ed547fef6831bdd15bba7e4e99ebb5a91d9a110
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-01-22 20:43:42 +0200

    Show casts in attach page

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,7 +1,10 @@
 import Cast from "../models/Cast.js";
 
 export default {
+    getAll() {
+        return Cast.find({});
+    },
     create(castData) {
         return Cast.create(castData);
-    }
+    },
 }
