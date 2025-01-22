045ba80fed28345c092d43a165caf764602ec3a9
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-01-22 18:56:36 +0200

    Refactor home page to use database movies

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -3,9 +3,15 @@ import movieService from '../services/movie-service.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    const movies = movieService.getAll();
+router.get('/', async (req, res) => {
+    // *Second solution use .lean on query to get plain object
+    const movies = await movieService.getAll();
 
+    // *First solution - convert documents to objects
+    // Convert documents to plain objects
+    // const plainMovies = movies.map(m => m.toObject());
+
+    // *Third solution is to use allowProtoPropertiesByDefault runtimeOption in handlebars
     res.render('home', { movies });
 });
 
