8386fc6e66fef1b96b4bac4ddcf0d4f921a09bca
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-01-29 21:17:49 +0200

    Delete movie

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -38,20 +38,10 @@ export default {
 
         return result;
     },
-    async attachCast(movieId, castId) {
-
-        // Attach #1
-        // const movie = await Movie.findById(movieId);
-        // if (movie.casts.includes(castId)) {
-        //     return;
-        // }
-
-        // movie.casts.push(castId);
-        // await movie.save();
-
-        // return movie;
-
-        // Attach #2
+    attachCast(movieId, castId) {
         return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
-    }
+    },
+    delete(movieId) {
+        return Movie.findByIdAndDelete(movieId);
+    } 
 }
