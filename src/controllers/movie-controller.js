e95604bdb463fd6b6bbb5217c2f4332e9463a35b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-01-29 20:39:42 +0200

    Show details buttons if user is creator

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -28,8 +28,9 @@ movieController.get('/:movieId/details', async (req, res) => {
 
     const movieId = req.params.movieId;
     const movie = await movieService.getOneWithCasts(movieId);
-
-    res.render('movie/details', { movie });
+    const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
+    
+    res.render('movie/details', { movie, isCreator  });
 });
 
 movieController.get('/:movieId/attach-cast', async (req, res) => {
@@ -37,7 +38,7 @@ movieController.get('/:movieId/attach-cast', async (req, res) => {
     const movie = await movieService.getOne(movieId);
     const casts = await castService.getAll({exclude: movie.casts});
 
-    res.render('movie/attach-cast', { movie, casts });
+    res.render('movie/attach-cast', { movie, casts});
 });
 
 movieController.post('/:movieId/attach-cast', async (req, res) => {
