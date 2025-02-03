d5c5930f2221ec3253136a7f1cb8af8e78351dba
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-02-03 21:42:23 +0200

    Show error notification on register page

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -15,7 +15,13 @@ authController.post('/register', async (req, res) => {
     try {
         await authService.register(userData);
     } catch (err) {
-        console.log(getErrorMessage(err));
+        const error = getErrorMessage(err);
+
+        // Show error on the page
+        // Return to register page
+        return res.render('auth/register', { error })
+
+        // return res.redirect('/auth/register');
     }
 
     res.redirect('/auth/login');
